e691ff4bf8d662ff3e7cec7eaac466213a7b6e8b
pinsight25 <pinsightindia@gmail.com>
2025-07-18 10:26:17 +0300

    Fix: Remove duplicate QueryClientProvider to resolve navigation and loading issues

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,9 @@ import React from 'react';
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>
 );
